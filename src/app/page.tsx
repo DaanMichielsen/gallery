c504ffa45cea08684a0da263801aedf94e5b6616
DaanMichielsen <r0896365@student.thomasmore.be>
2024-04-17 20:28:39 +0200

    scaffold

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
+import { url } from "inspector";
 import Link from "next/link";
+
+const images = [
+  "https://utfs.io/f/610c9bb9-a29a-47bb-ac21-7c3cb07ee6f3-1r5u.svg",
+  "https://utfs.io/f/b03c3780-4bc9-4eee-b957-bc2253884580-eouppx.svg",
+  "https://utfs.io/f/a11be848-b5f5-43a0-a71d-2f6f1213544b-1p3s.svg",
+  "https://utfs.io/f/47ca2bc3-5bbe-43d3-adb8-5feaafe26735-7wv6ug.svg",
+];
+
+const mockImages = images.map((image, index) => ({
+  id: index,
+  url: image,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map(
+          (image) => (
+            <div key={image.id} className="w-48">
+              <img src={image.url} alt="image" className="w-full" />
+            </div>
+          ),
+        )}
+      </div>
       Gallery in progress
     </main>
   );
